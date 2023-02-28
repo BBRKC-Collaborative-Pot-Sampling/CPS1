e11b9b2459a9aaca51c9f8a8e2d8b24b3f04d121
Emily Ryznar <emily.ryznar@nmfs.local>
2023-02-28 14:52:14 -0900

    Adding error checking code

--- Codes/R/WinterSurvey_2023_processing_ER.R
+++ Codes/R/WinterSurvey_2023_processing_ER.R
@@ -11,17 +11,13 @@ library(terra)
 library(rgdal)
 library(colorRamps)
 
+options(dplyr.summarise.inform = FALSE)
+
 # LOAD DATA -------------------------------------------------------------------------------------------
   
   # Load summary catch and specimen tables
       catch <- list.files("./Data/FTP/Catch - FTP/") %>%
         purrr::map_df(~read.csv(paste0("./Data/FTP/Catch - FTP/", .x)))
-      
-      # Process catch data for Oracle and save
-      catch %>%
-        dplyr::select(HAUL_ID, SPECIES_CODE, NUMBER_CRAB, VESSEL, CRUISE, HAUL) %>%
-        write.csv("./DataforOracle/Processed_Catch_Summary.csv")
-      
       
       specimen <- list.files("./Data/FTP/Specimen - FTP/") %>%
         purrr::map_df(~read.csv(paste0("./Data/FTP/Specimen - FTP/", .x)))
@@ -92,7 +88,7 @@ library(colorRamps)
       samples <- right_join(raw_sample, raw_sample_values) %>%
         dplyr::select(HAUL_ID, CATCH_SAMPLE_ID, SPECIES_CODE, SPECIES_NAME, SEX, TOSSED)
     
-  # Expand specimen biometric table, join to raw_specimen table to get catch sample ID, join with samples file to get 
+  # Expand specimen biometric table, join to raw_specimen table to get catch sample ID, join with samples file to add
   # number tossed
       raw_specimen_bio %>%
         dplyr::select(HAUL_ID, SPECIMEN_ID, BIOMETRIC_NAME, VALUE) %>%
@@ -121,17 +117,30 @@ library(colorRamps)
         dplyr::select(CRUISE, VESSEL, POT, BUOY, LAT_DD, LON_DD, DATE_HAUL, TIME_HAUL, SOAK_TIME, DEPTH_F,
                       SPECIES_CODE, SEX, LENGTH, WIDTH, SAMPLING_FACTOR, SHELL_CONDITION, EGG_COLOR, EGG_CONDITION, 
                       CLUTCH_SIZE, WEIGHT, DISEASE_CODE, DISEASE_DORSAL, DISEASE_VENTRAL, DISEASE_LEGS,  
-                      CHELA_HEIGHT, MERUS_LENGTH, COMMENTS) -> catch_summary
+                      CHELA_HEIGHT, MERUS_LENGTH, COMMENTS) -> specimen_table
       
-      # Process catch_summary for Oracle, save
-      catch_summary %>%
+      # Process specimen table for Oracle, save
+      specimen_table %>%
         dplyr::select(!c(LAT_DD, LON_DD, DATE_HAUL, TIME_HAUL, SOAK_TIME, DEPTH_F)) %>%
         dplyr::rename(HAUL = POT, STATION = BUOY) %>%
         write.csv("./DataForOracle/Processed_Specimen_Data.csv")
-   
+  
+   # Update catch summary table with new crab #s from sampling factor
+    specimen_table %>%
+        group_by(CRUISE, VESSEL, POT, SPECIES_CODE) %>%
+        summarise(NUMBER_CRAB = sum(SAMPLING_FACTOR)) %>%
+        right_join(catch %>% rename(POT = HAUL, N_ENTRIES = NUMBER_CRAB)) %>%
+         dplyr::select(HAUL_ID, SPECIES_CODE, CRUISE, VESSEL, POT, NUMBER_CRAB, N_ENTRIES) -> catch_summary
+       
+      # Process catch_summary table for Oracle, save
+      catch_summary %>%
+        dplyr::select(!N_ENTRIES) %>%
+        dplyr::rename(HAUL = POT) %>%
+        write.csv("./DataForOracle/Processed_Catch_Summary.csv")
+        
     
   # Make non-overlapping maturity/sex and legal/sublegal categories, bind together
-      catch_summary %>%
+      specimen_table %>%
         mutate(MAT_SEX = case_when((SPECIES_CODE == 69322 & SEX == 1 & LENGTH >= 120) ~ "Mature male",
                                    (SPECIES_CODE == 69322 & SEX == 1 & LENGTH < 120) ~ "Immature male",
                                    (SPECIES_CODE == 69322 & SEX == 2 & CLUTCH_SIZE >= 1) ~ "Mature female",
@@ -166,6 +175,220 @@ library(colorRamps)
       
       # Save csv
       write.csv(pot_cpue, "./Output/W2023_potcpue.csv")
+
+# ERROR CHECKING -----------------------------------------------------------------------------------------------------
+  
+  # Write function
+  error_chk <- function(specimen_table, catch_summary){
+    
+    print("CHECKING VESSEL AND CRUISE...")
+    
+      # 1) Does cruise number match 202201?"
+      if(unique(specimen_table$CRUISE == 202201) == FALSE){
+        print("ERROR: wrong cruise number")
+      }
+
+      
+      # "2) Does the vessel # match the vessels utilized in the survey?"
+      if(unique(specimen_table$VESSEL) %in% c(162, 164) == FALSE){
+        print("ERROR: vessel numbers entered do not match survey vessels")
+      }
+      
+      
+      print("Inventory of catch by cruise and vessel")
+      specimen_table %>%
+        group_by(VESSEL, CRUISE) %>%
+        summarise(COUNT = sum(SAMPLING_FACTOR),
+                  N = n()) %>%
+        as.data.frame() -> invent1
+      print(invent1)
+    
+    print("CHECKING SPECIES CODES AND SEX...")
+    
+      # 3) Do species codes match RKC code 69322?"
+      if(unique(specimen_table$SPECIES_CODE == 69322) == FALSE){
+        print("ERROR: wrong code entered for RKC")
+      }
+    
+      
+      # 4) Are sex codes assigned to either 1 or 2?"
+      if(unique(specimen_table$SEX %in% c(1:2)) == FALSE){
+        print("ERROR: sex code not 1 or 2")
+      }
+      
+      print("Inventory of catch by cruise, vessel, species code, and sex")
+      specimen_table %>%
+        group_by(VESSEL, CRUISE, SPECIES_CODE, SEX) %>%
+        summarise(COUNT = sum(SAMPLING_FACTOR), 
+                  N = n()) %>%
+        as.data.frame() -> invent2
+      print(invent2)
+    
+    
+    print("CHECKING SHELL CONDITION, EGG COLOR, EGG CONDITION, CLUTCH_SIZE...")
+      
+      #5) Are egg color codes valid for females?"
+      if(unique(filter(specimen_table, SEX != 1)$EGG_COLOR %in% c(0, 2:6)) == FALSE){
+        print("ERROR: invalid female egg color code (not 0 or 2:6)")
+      }
+      
+      #6) Are egg condition codes valid for females?"
+      if(unique(filter(specimen_table, SEX != 1)$EGG_CONDITION %in% c(0:4) == FALSE)){
+        print("ERROR: invalid female egg condition code (not 0:4)")
+      } 
+      
+      # 7) Are clutch size codes valid for females?
+      if(unique(filter(specimen_table, SEX != 1)$CLUTCH_SIZE %in% c(0:6)) == FALSE){
+        print("ERROR: invalid female clutch size code (not 0:6)")
+      }
+      
+      # 8) Any egg, egg condition, or clutch size codes assigned to males?
+      if(unique((specimen_table$SEX == 1 & is.na(specimen_table$EGG_COLOR | specimen_table$EGG_CONDITION | 
+                                                 specimen_table$CLUTCH_SIZE) == FALSE))== TRUE){
+        print("ERROR: egg, egg condition, or clutch size code assigned to male")
+      } 
+         
+      
+      # 9) Any questionable egg condition x shell condition combinations for females?"
+      
+      # Checking shell condition = 0 and egg condition = 1
+      if(unique(filter(specimen_table, SEX != 1)$SHELL_CONDITION == 0 & 
+               filter(specimen_table, SEX != 1)$EGG_CONDITION == 1) == TRUE){
+        print("ERROR: female with shell condition = 0 and egg condition = 1")
+      } 
+      
+      # Checking shell condition = 1 and egg condition >1
+      if(unique(filter(specimen_table, SEX != 1)$SHELL_CONDITION == 1 & 
+               filter(specimen_table, SEX != 1)$EGG_CONDITION > 1) == TRUE){
+        print("ERROR: female with shell condition = 1 and egg condition >1")
+      }
+      
+      # Checking shell condition = 3, 4, or 5 and egg condition = 1
+      if(unique(filter(specimen_table, SEX != 1)$SHELL_CONDITION %in% c(3:5) & 
+               filter(specimen_table, SEX != 1)$EGG_CONDITION == 1) == TRUE){
+        print("ERROR: female with shell condition 3:5 and egg condition = 1")
+      }
+      
+      # Checking shell condition = 1 and egg condition >=2
+      if(unique(filter(specimen_table, SEX != 1)$SHELL_CONDITION ==1 & 
+               filter(specimen_table, SEX != 1)$EGG_CONDITION >=2) == TRUE){
+        print("ERROR: female with shell condition = 1 and egg condition >=2")
+      }
+      
+      #10) Any females without egg color, egg condition, or clutch codes?
+      if(unique(specimen_table$SEX == 2 & (is.na(specimen_table$EGG_COLOR | specimen_table$EGG_CONDITION 
+                                                 | specimen_table$CLUTCH_SIZE) == TRUE)) == TRUE){
+        print("ERROR: female missing egg color, egg condition, or clutch code")
+      }
+             
+      
+      print("Inventory of shell condition")
+      specimen_table %>%
+        group_by(SPECIES_CODE, SEX, SHELL_CONDITION) %>%
+        summarise(COUNT = sum(SAMPLING_FACTOR),
+                  N = n()) %>%
+        as.data.frame() -> invent3
+      print(invent3)
+      
+      print("Inventory of female shell condition and egg codes")
+      specimen_table %>%
+        filter(SEX == 2) %>%
+        group_by(SPECIES_CODE, SHELL_CONDITION, EGG_COLOR, EGG_CONDITION, CLUTCH_SIZE) %>%
+        summarise(COUNT = sum(SAMPLING_FACTOR),
+                  N = n()) %>%
+        as.data.frame()-> invent4
+      print(invent4)
+    
+    print("CHECKING LENGTHS AND WIDTHS...")
+
+      # 11) Any missing lengths for RKC?"
+      if(unique(is.na(specimen_table$LENGTH)) == TRUE){
+        print("ERROR: missing length for RKC")
+      } 
+      
+      # 12) Any small female crab with a clutch size?"
+      if(unique(filter(specimen_table, SEX != 1)$LENGTH < 65 & 
+               filter(specimen_table, SEX != 1)$CLUTCH_SIZE > 0) == TRUE){
+        print("ERROR: female <65 with clutch size >0")
+      } 
+      
+      # 13) Any small crab with old shell condition?"
+      if(unique(specimen_table$LENGTH < 60 & specimen_table$SHELL_CONDITION > 2) == TRUE){
+        print("ERROR: crab < 60 with shell condition >2")
+      } 
+      
+      # 14) Any widths entered for RKC? 
+      if(unique(is.na(specimen_table$WIDTH)) == FALSE) {
+        print("ERROR: width entered for RKC when length is needed")
+      }
+      
+      print("What are the minimum and maximum lengths reported by sex?")
+      specimen_table %>%
+        group_by(SPECIES_CODE, SEX) %>%
+        summarise(MIN_LENGTH = min(LENGTH),
+                  MAX_LENGTH = max(LENGTH)) %>%
+        as.data.frame() -> A15
+      print(A15)
+    
+    print("CHECKING DISEASE CODES...")
+
+      # 16) Any black mat recorded but without % coverage entry?
+      unique(specimen_table$DISEASE_CODE == 1 & (is.na(specimen_table$DISEASE_DORSAL) == TRUE &
+                                                   is.na(specimen_table$DISEASE_LEGS) == TRUE &
+                                                   is.na(specimen_table$DISEASE_VENTRAL) == TRUE)) -> A16
+      print(A16)
+      
+      print("17) Any bitter crab recorded for RKC and/or any bitter crab recorded with entries in % coverage?")
+      unique(specimen_table$DISEASE_CODE == 2 |
+               specimen_table$DISEASE_CODE == 2 & (is.na(specimen_table$DISEASE_DORSAL) == FALSE &
+                                                     is.na(specimen_table$DISEASE_LEGS) == FALSE &
+                                                     is.na(specimen_table$DISEASE_VENTRAL) == FALSE)) -> A17
+      print(A17)
+      
+      print("18) Any disease code not recorded but entries in % coverage?")
+      unique(is.na(specimen_table$DISEASE_CODE) == TRUE & (is.na(specimen_table$DISEASE_DORSAL) == FALSE &
+                                                             is.na(specimen_table$DISEASE_LEGS) == FALSE &
+                                                             is.na(specimen_table$DISEASE_VENTRAL) == FALSE)) -> A18
+      print(A18)
+      
+      print("19) Any disease codes >9?")
+      unique(specimen_table$DISEASE_CODE>9) -> A19
+      print(A19)
+    
+    print("CHECKING SAMPLING FACTOR")
+    
+      print("20) What is the maximum sampling factor by by sex?")
+      specimen_table %>%
+        group_by(SPECIES_CODE, SEX) %>%
+        summarise(MIN_SAMPLING_FACTOR = min(SAMPLING_FACTOR),
+                  MAX_SAMPLING_FACTOR= max(SAMPLING_FACTOR)) %>%
+        as.data.frame() -> A20
+      print(A20)
+      
+      print("21) Any sampling factors < 1?")
+      unique(specimen_table$SAMPLING_FACTOR < 1) -> A21
+      print(A21)
+    
+    # Check station ids (bouys)
+      
+    print("COMPARING SPECIMEN TABLE WITH CATCH SUMMARY")
+    specimen_table %>%
+      group_by(CRUISE, VESSEL, POT, SPECIES_CODE) %>%
+      summarise(NUMBER_CRAB = sum(SAMPLING_FACTOR),
+                N_ENTRIES = n()) -> spec_sum
+    
+    catch_summary %>%
+      dplyr::select(CRUISE, VESSEL, POT, SPECIES_CODE, NUMBER_CRAB, N_ENTRIES) -> catch_sum
+    
+    print("22) Does the number of crab and number of entries match between the specimen table and catch summary?")
+    all.equal(spec_sum, catch_sum) -> A22
+    print(A22)
+  }    
+  
+  # Run function
+  error_chk(specimen_table, catch_summary) 
+  
+
       
 # MAP BBRKC CPUE --------------------------------------------------------------------------------------------------------------------
     
